1f3ddf51bad947069efd7fb44564e4d1b9651e55
Aadya Sharma <aadya1773sharma@gmail.com>
2025-05-08 11:55:43 +0530

    do

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './components/App';
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
 
-
-ReactDOM.render(<App />, document.getElementById("root"));
